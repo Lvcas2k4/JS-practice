b4566d2ba12a0ae51bdbc99d9736118ee8d79f4d
Lucas <137804016+Lvcas2k4@users.noreply.github.com>
2023-08-07 12:59:03 -0300

    Inicio 2

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
 
-function calcPrice{
-    const disc = (price * discount)
+function calcPrice(precio,descuento){
+    const disc = (precio * descuento)
+    const total= precio-descuento
+    return total
+}
+function calcCuotas(precio, cuotas){
+    return precio/cuotas
+}
+
+function MakeProd(titulo,precio,descuento,cuotas,descripcion){
+return`
+Titulo: ${titulo}
+Precio: $${precio}
+Descuento: el descuento es de ${descuento}
+Precio con Descuento: $${calcPrice(precio,descuento)}
+Descripcion:${descripcion}
+`
 }
