c01354a074a86470e63e06dca8a81991692345e9
Lucas <137804016+Lvcas2k4@users.noreply.github.com>
2023-08-08 11:05:33 -0300

    add arrays

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function calcPrice(precio,descuento){
+/*function calcPrice(precio,descuento){
     const disc = (precio * descuento)
     const total= precio-descuento
     return total
@@ -17,4 +17,21 @@ Descuento: el descuento es de ${descuento}
 Precio con Descuento: $${calcPrice(precio,descuento)}
 Descripcion:${descripcion}
 `
+}*/
+
+let producto1 = {
+    nombre : 'pes 06',
+    precio : '5$'
 }
+
+let producto2 = {
+    nombre : 'COD BO',
+    precio : '10$'
+}
+let miArray = [producto1,producto2]
+
+console.log(miArray)
+
+let array2 = [];
+array2.push(producto1,producto2)
+console.log(array2)
